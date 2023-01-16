c53e2ad8b7b3bbc7ca55ccf7798d17b5d69a26f7
Your Name <you@example.com>
2023-01-16 16:39:32 +0530

    test

---
+++ src/main/java/com/sajithten/springbootjavamaven/SpringbootJavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.sajithten.springbootjavamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajithten/springbootjavamaven/SpringbootJavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajithten.springbootjavamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
